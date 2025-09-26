0ad9ba60f6fc3ac3d7ddfd4d1fbaad0750e51c77
Evgenii Stetsenko <i@estetsenko.com>
2025-09-26 17:22:12 +0300

    feat: it

--- src/main/java/com/estetsenko/App.java
+++ src/main/java/com/estetsenko/App.java
@@ -8,4 +8,8 @@ public class App {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
 }
--- src/test/java/com/estetsenko/AppTest.java
+++ src/test/java/com/estetsenko/AppTest.java
@@ -10,4 +10,9 @@ class AppTest {
     void add() {
         assertEquals(5, App.add(2, 3), "2 + 3 should equal 5");
     }
+
+    @Test
+    void mul() {
+        assertEquals(6, App.mul(2, 3), "2 * 3 should equal 6");
+    }
 }
